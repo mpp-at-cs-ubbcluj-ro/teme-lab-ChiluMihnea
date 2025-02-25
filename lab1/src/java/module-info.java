aeede6f91faca630c819ec81a714d239f2bb5557
cmihnea <chiligaming1804@gmail.com>
2025-02-25 08:55:06 +0200

    test2

--- lab1/src/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ubb.scs.map.lab1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ubb.scs.map.lab1 to javafx.fxml;
-    exports ubb.scs.map.lab1;
-}
--- lab1/src/java/ubb/scs/map/lab1/HelloApplication.java
+++
@@ -1,7 +0,0 @@
-package src.java.ubb.scs.map.lab1;
-
-public class HelloApplication{
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
